a97a23e201c24a366afa17e59105863300eb5060
chuntley56 <coreyhuntley562@gmail.com>
2018-08-14 10:55:35 -0700

    add copy

--- src/index.js
+++ src/index.js
@@ -11,22 +11,30 @@ class App extends Component {
 constructor(props){
   super(props);
 
-  this.state = { videos: [] };
+  this.state = {
+    videos: [],
+    selectedVideo: null
+   };
 
-  YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-    this.setState({ videos });
+  YTSearch({key: API_KEY, term: 'dollar shave club'}, (videos) => {
+    this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+     });
   });
 }
 
   render() {
     return (<div>
       <SearchBar />
-      <VideoDetail video={this.state.videos[0]} />
+      <VideoDetail video={this.state.selectedVideo} />
       <VideoList videos={this.state.videos} />
       </div>
       );
   }
 }
+
+
 
 
 // take this generated html and insert into DOM
